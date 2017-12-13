15f1a01f5e84d238ae2357b9bbd5e46294684d3b
Anthony <anthony_bouillon@outlook.fr>
2017-12-13 08:36:31 +0100

    Les variables

--- index.php
+++ index.php
@@ -1,7 +1,16 @@
-<?php
-$string = 'Ma chaine de caractère';
-$int = 12;
-$float = 12.5;
-$boleen = true;
-echo $string. ' ' .$int. ' ' .$float. ' ' .$boleen;
- ?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Exercice 4</title>
+  </head>
+  <body>
+    <?php
+    $string = 'Ma chaine de caractère';
+    $int = 12;
+    $float = 12.5;
+    $boleen = true;
+    echo $string. ' ' .$int. ' ' .$float. ' ' .$boleen;
+    ?>
+  </body>
+</html>
